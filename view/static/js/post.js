d9fb6e54cbc480a1eed7f10464a71cdfcf09b00e
Creata21 <yerezhepabdulla@gmail.com>
2023-05-16 16:08:31 +0600

    JS

--- view/static/js/post.js
+++ view/static/js/post.js
@@ -1,29 +1,36 @@
 // add comment under post
-let postId = document.getElementById("id")
+let postID = document.getElementById("id").innerText
+let userID = document.getElementById("user_id").innerText
+console.log(postID, userID);
 let addCommentButton = document.getElementById("addCommentBtn")
 addCommentButton.addEventListener("click", ()=>{
-    submitNewComment(postId)
+    if (userID.length == 0){
+        signInPrompt()
+    }else{
+        submitNewComment(postID)
+    }
 })
 
-// function editComment(id) {
-    //     document.getElementById("comment-content-" + id).style.display = "none";
-    //     document.getElementById("edit-comment-" + id).style.display = "block";
-    // }
-    
-function submitNewComment(id) {
+function signInPrompt() {
+    signInContainer.classList.remove("hidden");
+    overlay.style.display = "block"
+}
+
+
+function submitNewComment(p_id) {
     let content = document.getElementById("new-comment").value;
     fetch("/post/comment", {
             method: "POST",
-            body: JSON.stringify({ post_id: id, content: content }),
+            body: JSON.stringify({ post_id: p_id, content: content }),
             headers: {
             "Content-Type": "application/json"
         }
     })
     .then(response => {
         if (response.ok) {
-        location.reload()
+            location.reload()
         } else {
-        console.error("Failed to update comment");
+            console.error("Failed to update comment");
         }
     })
     .catch(error => {
